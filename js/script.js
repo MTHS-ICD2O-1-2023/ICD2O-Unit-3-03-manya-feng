112ab39cf5803bde327570ba427634b4eafef655
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-04-09 18:49:17 +0000

    change code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Manya All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Manya
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of a sphere.
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const radiusOfSphere = parseFloat(document.getElementById("radius-of-sphere").value)
+
+  // process
+  const volumeOfSphere = radiusOfSphere * Math.PI * 4/3
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volumeOfSphere.toFixed(2) + ' mm³'
+}
